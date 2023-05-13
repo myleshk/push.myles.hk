1b99363e45b2611c1fbe7636e53d83b1ab8ef83c
Myles Fang <git@myles.hk>
2023-05-13 23:49:26 +0800

    feat: add build folder (docs)

---
+++ docs/precache-manifest.ec59cd3d7ad6f9274b22fc085b8ec4c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a941eff343748d985de",
+    "url": "/static/css/main.bf140798.chunk.css"
+  },
+  {
+    "revision": "7a941eff343748d985de",
+    "url": "/static/js/main.7a941eff.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "304185b26500d5844dfd",
+    "url": "/static/js/2.304185b2.chunk.js"
+  },
+  {
+    "revision": "1f46550c42e737540ce6",
+    "url": "/static/js/runtime~main.1f46550c.js"
+  },
+  {
+    "revision": "7132cb1b1f23ccd51cca1ce9a36c1d99",
+    "url": "/static/media/logo.7132cb1b.png"
+  },
+  {
+    "revision": "f8956ca001b64656c0a9c2dc5cacf95e",
+    "url": "/static/media/phone.f8956ca0.png"
+  },
+  {
+    "revision": "47a6e80a0249ce24819aad46dc7e848c",
+    "url": "/index.html"
+  }
+];
