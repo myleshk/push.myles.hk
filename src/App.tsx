505f51b67ec0d6f72925e9e43241a763de09d24e
Myles Fang <git@myles.hk>
2023-05-13 23:06:02 +0800

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
+import Phone from './compoents/Phone';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <img src={logo}
+        alt="Logo"
+        width="106"
+        height="36"
+      />
+
+      <Phone />
+
+      <a className="download-btn" href="https://article.zlink.toutiao.com/d13Q">下载今日头条</a>
+    </main>
   );
 }
 
---
+++ src/compoents/Phone.tsx
@@ -0,0 +1,18 @@
+import phoneImage from '../assets/phone.png';
+
+export default function Phone() {
+    const message = "Hello World! 3";
+
+    return (
+        <div className="phone">
+            <img
+                src={phoneImage}
+                alt="Phone"
+                width="190"
+                height="410"
+            />
+            <span className="message">
+                {message}
+            </span>
+        </div>)
+}
